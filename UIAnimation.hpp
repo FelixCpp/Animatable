b8d05f8d6f70fcd46ef78324fc9dc1e9ff6f62f3
FelixCpp <fix.busch@gmail.com>
2023-04-19 00:56:03 +0200

    Timing curve added

--- UIAnimation.hpp
+++ UIAnimation.hpp
@@ -11,7 +11,8 @@
 #include <type_traits> // std::move
 #include <memory> // std::unique_ptr
 #include <algorithm> // std::clamp
-#include <numbers>
+#include <numbers> // std::numbers::pi_v<float>
+#include <vector> // std::vector
 
 namespace ui
 {
@@ -21,69 +22,56 @@ namespace ui
 		{
 			return x;
 		}
-
 		inline float InSine(float x)
 		{
 			return 1.0f - std::cos((x * std::numbers::pi_v<float>) / 2.0f);
 		}
-
 		inline float OutSine(float x)
 		{
 			return std::sin((x * std::numbers::pi_v<float>) / 2.0f);
 		}
-
 		inline float InOutSine(float x)
 		{
 			return -(std::cos(std::numbers::pi_v<float> * x) - 1.0f) / 2.0f;
 		}
-
 		inline float InCubic(float x)
 		{
 			return x * x * x;
 		}
-
 		inline float OutCubic(float x)
 		{
 			return 1.0f - std::pow(1.0f - x, 3.0f);
 		}
-
 		inline float InOutCubic(float x)
 		{
 			return x < 0.5f ? 4.0f * x * x * x : 1.0f - std::pow(-2.0f * x + 2.0f, 3.0f) / 2.0f;
 		}
-
 		inline float InQuint(float x)
 		{
 			return x * x * x * x * x;
 		}
-
 		inline float OutQuint(float x)
 		{
 			return 1.0f - std::pow(1.0f - x, 5.0f);
 		}
-
 		inline float InOutQuint(float x)
 		{
 			return x < 0.5f ? 16.0f * x * x * x * x * x : 1.0f - std::pow(-2.0f * x + 2.0f, 5.0f) / 2.0f;
 		}
-
 		inline float InCirc(float x)
 		{
 			return 1.0f - std::sqrt(1.0f - std::pow(x, 2.0f));
 		}
-
 		inline float OutCirc(float x)
 		{
 			return std::sqrt(1.0f - std::pow(x - 1.0f, 2.0f));
 		}
-
 		inline float InOutCirc(float x)
 		{
 			return x < 0.5f
 				? (1.0f - std::sqrt(1.0f - std::pow(2.0f * x, 2.0f))) / 2.0f
 				: (std::sqrt(1.0f - std::pow(-2.0f * x + 2.0f, 2.0f)) + 1.0f) / 2.0f;
 		}
-
 		inline float InElastic(float x)
 		{
 			static constexpr float c4 = (2.0f * std::numbers::pi_v<float>) / 3.0f;
@@ -94,7 +82,6 @@ namespace ui
 				? 1.0f
 				: -std::pow(2.0f, 10.0f * x - 10.0f) * std::sin((x * 10.0f - 10.75f) * c4);
 		}
-
 		inline float OutElastic(float x)
 		{
 			static constexpr float c4 = (2.0f * std::numbers::pi_v<float>) / 3.0f;
@@ -105,7 +92,6 @@ namespace ui
 				? 1.0f
 				: pow(2.0f, -10.0f * x) * sin((x * 10.0f - 0.75f) * c4) + 1.0f;
 		}
-
 		inline float InOutElastic(float x)
 		{
 			static constexpr float c5 = (2.0f * std::numbers::pi_v<float>) / 4.5f;
@@ -118,47 +104,38 @@ namespace ui
 				? -(std::pow(2.0f, 20.0f * x - 10.0f) * std::sin((20.0f * x - 11.125f) * c5)) / 2.0f
 				: (std::pow(2.0f, -20.0f * x + 10.0f) * std::sin((20.0f * x - 11.125f) * c5)) / 2.0f + 1.0f;
 		}
-
 		inline float InQuad(float x)
 		{
 			return x * x;
 		}
-
 		inline float OutQuad(float x)
 		{
 			return 1.0f - (1.0f - x) * (1.0f - x);
 		}
-
 		inline float InOutQuad(float x)
 		{
 			return x < 0.5f ? 2.0f * x * x : 1.0f - std::pow(-2.0f * x + 2.0f, 2.0f) / 2.0f;
 		}
-
 		inline float InQuart(float x)
 		{
 			return x * x * x * x;
 		}
-
 		inline float OutQuart(float x)
 		{
 			return 1.0f - std::pow(1.0f - x, 4.0f);
 		}
-
 		inline float InOutQuart(float x)
 		{
 			return x < 0.5f ? 8.0f * x * x * x * x : 1.0f - std::pow(-2.0f * x + 2.0f, 4.0f) / 2.0f;
 		}
-
 		inline float InExpo(float x)
 		{
 			return x == 0.0f ? 0.0f : std::pow(2.0f, 10.0f * x - 10.0f);
 		}
-
 		inline float OutExpo(float x)
 		{
 			return x == 1.0f ? 1.0f : 1.0f - std::pow(2.0f, -10.0f * x);
 		}
-
 		inline float InOutExpo(float x)
 		{
 			return x == 0.0f
@@ -168,7 +145,6 @@ namespace ui
 				: x < 0.5f ? pow(2.0f, 20.0f * x - 10.0f) / 2.0f
 				: (2.0f - pow(2.0f, -20.0f * x + 10.0f)) / 2.0f;
 		}
-
 		inline float InBack(float x)
 		{
 			static constexpr float c1 = 1.70158f;
@@ -176,14 +152,12 @@ namespace ui
 
 			return c3 * x * x * x - c1 * x * x;
 		}
-
 		inline float OutBack(float x)
 		{
 			static constexpr float c1 = 1.70158f;
 			static constexpr float c3 = c1 + 1.0f;
 			return 1.0f + c3 * std::pow(x - 1.0f, 3.0f) + c1 * std::pow(x - 1.0f, 2.0f);
 		}
-
 		inline float InOutBack(float x)
 		{
 			static constexpr float c1 = 1.70158f;
@@ -193,7 +167,6 @@ namespace ui
 				? (std::pow(2.0f * x, 2.0f) * ((c2 + 1.0f) * 2.0f * x - c2)) / 2.0f
 				: (std::pow(2.0f * x - 2.0f, 2.0f) * ((c2 + 1.0f) * (x * 2.0f - 2.0f) + c2) + 2.0f) / 2.0f;
 		}
-
 		inline float OutBounce(float x)
 		{
 			static constexpr float n1 = 7.5625f;
@@ -204,21 +177,19 @@ namespace ui
 			}
 
 			if (x < 2.0f / d1) {
-				return n1 * (x - 1.5f / d1) * x + 0.75f;
+				return n1 * (x -= 1.5f / d1) * x + 0.75f;
 			}
 
 			if (x < 2.5f / d1) {
-				return n1 * (x - 2.25f / d1) * x + 0.9375f;
+				return n1 * (x -= 2.25f / d1) * x + 0.9375f;
 			}
-
-			return n1 * (x - 2.625f / d1) * x + 0.984375f;
+			
+			return n1 * (x -= 2.625f / d1) * x + 0.984375f;
 		}
-
 		inline float InBounce(float x)
 		{
 			return 1.0f - OutBounce(1.0f - x);
 		}
-
 		inline float InOutBounce(float x)
 		{
 			return x < 0.5f
@@ -570,6 +541,172 @@ namespace ui
 
 	};
 
+	class InstantAnimation
+	{
+	public:
+
+		constexpr InstantAnimation() = default;
+
+		bool IsDone() const
+		{
+			return true;
+		}
+
+		void OnDone() const
+		{
+		}
+
+		template <typename T>
+		void Update(const T& initialValue, const T& targetValue, const T& currentValue, float deltaTime)
+		{
+		}
+
+		template <typename T>
+		T GetValue(const T& initialValue, const T& targetValue, const T& currentValue) const
+		{
+			return targetValue;
+		}
+
+		void Repeat() const
+		{
+		}
+
+		void Reverse() const
+		{
+		}
+
+	};
+
+	class NeverAnimation
+	{
+	public:
+
+		constexpr NeverAnimation() = default;
+
+		bool IsDone() const
+		{
+			return false;
+		}
+
+		void OnDone() const
+		{
+		}
+
+		template <typename T>
+		void Update(const T& initialValue, const T& targetValue, const T& currentValue, float deltaTime)
+		{
+		}
+
+		template <typename T>
+		T GetValue(const T& initialValue, const T& targetValue, const T& currentValue) const
+		{
+			return initialValue;
+		}
+
+		void Repeat() const
+		{
+		}
+
+		void Reverse() const
+		{
+		}
+
+	};
+
+	class TimingCurveAnimation
+	{
+	public:
+
+		template <std::ranges::range TValues, std::ranges::range TFunctions>
+		constexpr explicit TimingCurveAnimation(float durationInSeconds, TValues values, TFunctions functions):
+			m_Values(std::ranges::begin(values), std::ranges::end(values)),
+			m_Functions(std::ranges::begin(functions), std::ranges::end(functions)),
+			m_DurationInSeconds(durationInSeconds),
+			m_Value(m_Values.empty() ? 0.0f : m_Values.front())
+		{
+		}
+
+		bool IsDone() const
+		{
+			return m_ValueIndex >= m_Values.size();
+		}
+
+		void OnDone() const
+		{
+		}
+
+		template <typename T>
+		void Update(const T& initialValue, const T& targetValue, const T& currentValue, float deltaTime)
+		{
+			float durationPerValue = m_DurationInSeconds / static_cast<float>(m_Values.size());
+			m_ElapsedTimeInSeconds = std::clamp(m_ElapsedTimeInSeconds + deltaTime, 0.0f, durationPerValue);
+
+			float progress = m_ElapsedTimeInSeconds / durationPerValue;
+			EaseFunction ease = GetFunction();
+			float previousTarget = GetPreviousTargetValue();
+			float target = GetTargetValue();
+			m_Value = std::lerp(previousTarget, target, ease(progress));
+
+			if (m_ElapsedTimeInSeconds >= durationPerValue)
+			{
+				++m_ValueIndex;
+				m_ElapsedTimeInSeconds = 0.0f;
+			}
+		}
+
+		template <typename T>
+		T GetValue(const T& initialValue, const T& targetValue, const T& currentValue) const
+		{
+			return initialValue + (targetValue - initialValue) * m_Value;
+		}
+
+		void Repeat()
+		{
+			m_ValueIndex = 1;
+		}
+
+		void Reverse()
+		{
+			std::ranges::reverse(m_Values);
+			std::ranges::reverse(m_Functions);
+		}
+
+	private:
+
+		float GetPreviousTargetValue() const
+		{
+			if (m_Values.empty())
+				return 0.0f;
+
+			return m_Values[std::min(m_ValueIndex - 1, m_Values.size() - 1)];
+		}
+
+		float GetTargetValue() const
+		{
+			if (m_Values.empty())
+				return 0.0f;
+
+			return m_Values[std::min(m_ValueIndex, m_Values.size() - 1)];
+		}
+
+		EaseFunction GetFunction() const
+		{
+			if (m_Functions.empty())
+				return &ease::Linear;
+
+			return m_Functions[std::min(m_ValueIndex - 1, m_Functions.size() - 1)];
+		}
+
+		std::vector<float> m_Values;
+		std::vector<EaseFunction> m_Functions;
+
+		float m_ElapsedTimeInSeconds = 0.0f;
+		float m_DurationInSeconds;
+		std::size_t m_ValueIndex = 1;
+		float m_Value;
+
+	};
+
 	constexpr AnimationBuilder<TimedAnimation> EaseLinear(float durationInSeconds = TimedAnimation::DefaultDuration)
 	{
 		return AnimationBuilder(
@@ -849,84 +986,31 @@ namespace ui
 			)
 		);
 	}
-	
-	class InstantAnimation
-	{
-	public:
-
-		bool IsDone() const
-		{
-			return true;
-		}
-
-		void OnDone() const
-		{
-		}
-
-		template <typename T>
-		void Update(const T& initialValue, const T& targetValue, const T& currentValue, float deltaTime)
-		{
-		}
-
-		template <typename T>
-		T GetValue(const T& initialValue, const T& targetValue, const T& currentValue) const
-		{
-			return targetValue;
-		}
-
-		void Repeat() const
-		{
-		}
-
-		void Reverse() const
-		{
-		}
-
-	};
-
 	constexpr AnimationBuilder<InstantAnimation> Instant()
 	{
 		return AnimationBuilder(InstantAnimation());
 	}
-
-	class NeverAnimation
-	{
-	public:
-
-		bool IsDone() const
-		{
-			return false;
-		}
-
-		void OnDone() const
-		{
-		}
-
-		template <typename T>
-		void Update(const T& initialValue, const T& targetValue, const T& currentValue, float deltaTime)
-		{
-		}
-
-		template <typename T>
-		T GetValue(const T& initialValue, const T& targetValue, const T& currentValue) const
-		{
-			return initialValue;
-		}
-
-		void Repeat() const
-		{
-		}
-
-		void Reverse() const
-		{
-		}
-
-	};
-
 	constexpr AnimationBuilder<NeverAnimation> Never()
 	{
 		return AnimationBuilder(NeverAnimation());
 	}
+
+	template <std::ranges::range TValues, std::ranges::range TFunctions>
+	constexpr AnimationBuilder<TimingCurveAnimation> TimingCurve(
+		float durationInSeconds,
+		TValues values,
+		TFunctions functions
+	)
+	{
+		return AnimationBuilder<TimingCurveAnimation>(
+			TimingCurveAnimation(
+				durationInSeconds,
+				std::move(values),
+				std::move(functions)
+			)
+		);
+	}
+	
 	
 	template <typename T>
 	class Animatable
@@ -984,7 +1068,7 @@ namespace ui
 
 	public:
 
-		Animatable(const T& initialValue = T{}):
+		constexpr Animatable(const T& initialValue = T{}):
 			m_InitialValue(initialValue),
 			m_TargetValue(initialValue),
 			m_CurrentValue(initialValue),
@@ -999,6 +1083,14 @@ namespace ui
 			m_TargetValue = targetValue;
 
 			m_Animation.reset(new AnimationWrapperImpl<TAnimation>(builder.GetAnimation()));
+		}
+
+		void WithoutAnimation(const T& targetValue)
+		{
+			m_InitialValue = targetValue;
+			m_TargetValue = targetValue;
+			m_CurrentValue = targetValue;
+			m_Animation.reset();
 		}
 
 		void Update(float deltaTime)
