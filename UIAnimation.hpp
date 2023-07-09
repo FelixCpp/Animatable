35aef7a1ae5fe7816d899ea436c8030886809ace
FelixCpp <fix.busch@gmail.com>
2023-07-09 22:20:12 +0200

    Predfined header removed

--- UIAnimation.hpp
+++ UIAnimation.hpp
@@ -1,11 +1,3 @@
-// 
-// Animatable.hpp
-// Pixels
-// 
-// Created by Felix Busch on 17.04.2023.
-// Copyright © 2023 Felix Busch. All rights reserved.
-// 
-
 #pragma once
 
 #include <type_traits> // std::move
